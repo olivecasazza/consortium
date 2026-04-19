480256d433602e3057772f6c6169e8840613cb8b
olivecasazza <olivecasazza@users.noreply.github.com>
2026-04-19 08:00:44 +0000

    deploy: 3d2ca1c556352cfcbb6c756fb6e5e78eb8fe9ccd

--- latest/trait.impl/core/ops/drop/trait.Drop.js
+++ latest/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["consortium_test_harness",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"consortium_test_harness/struct.DockerCluster.html\" title=\"struct consortium_test_harness::DockerCluster\">DockerCluster</a>",0]]]]);
+    const implementors = Object.fromEntries([["consortium_test_harness",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"consortium_test_harness/struct.DockerCluster.html\" title=\"struct consortium_test_harness::DockerCluster\">DockerCluster</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
